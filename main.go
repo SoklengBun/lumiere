6d85723362159270723b309532369ab76486e1d1
Sokleng Bun <soklengbun22@gmail.com>
2023-05-05 18:03:41 +0700

    add gitignore

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 func main(){
 	app := fiber.New()
 	app.Get("/api", welcome)
+	app.Post("/api/login", login)
 
 	log.Fatal(app.Listen(":3000"))
 }
 
 func welcome(c *fiber.Ctx) error {
 	return c.SendString("Hey Hey! Shinomiya San")
+}
+
+func login(c *fiber.Ctx) error {
+	return c.SendString("Hey Hey! Shinomiya San login")
 }
